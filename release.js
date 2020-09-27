a27cc6e4c9faf912233eff6f1735ae1cfa44b2ad
Louis Bertson <louisbertson@localhost.localdomain>
2020-09-27 05:52:02 +0200

    make it better again

--- release.js
+++ release.js
@@ -4,12 +4,9 @@
 // https://www.totaljs.com
 // ===================================================
 
-const options = {};
-
-// options.ip = '127.0.0.1';
-// options.port = parseInt(process.argv[2]);
-// options.config = { name: 'Total.js' };
-// options.sleep = 3000;
-
-require('total.js').http('release', options);
-// require('total.js').cluster.http(5, 'release', options);
+var fs = require('fs');
+var options = {};
+options.ip = 'auto';
+options.port = process.env.PORT;
+var framework = require('total.js');
+framework.http('release', options);
